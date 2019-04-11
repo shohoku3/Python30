f4a9d2d47b6674f1c5c5e1d419fb844a5cb48dd8
shohoku3 <syf19961002@outlook.com>
2019-04-11 10:33:15 +0800

    demo4

---
+++ demo5.py
@@ -0,0 +1,13 @@
+#list concation 
+catNames=[] # 初始化列表
+
+while True:
+    print('Enter the name of cat' + str(len(catNames)+1))
+    name=input()
+    if name == '':
+        break
+    catNames=catNames+[name] #list concatentaion
+print('the cat names are:')
+for name in catNames:
+    print(' '+name)
+
